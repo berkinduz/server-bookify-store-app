fd604c9cb44764afee15a5aa5290bc0490e66840
Berkin_Duz <berkin_duz@epam.com>
2023-11-01 18:25:49 +0300

    Revert "task5 changes"
    This reverts commit e4720524d4a0c350b031f60983d995774bf55938.

--- bookify-product-service/src/functions/importFileParser/schema.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  type: "object",
-  properties: {
-    data: { type: "object" },
-  },
-} as const;
--- bookify-product-service/src/functions/importProductsFile/schema.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  type: "object",
-  properties: {
-    data: { type: "object" },
-    message: { type: "string" },
-  },
-} as const;
